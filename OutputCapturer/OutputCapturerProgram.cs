c9b80d9591fe2972f55f70da46800b71f574638d
JeremyTCD <jeremytanchongdao@gmail.com>
2017-06-16 20:08:49 +0800

    Initial

---
+++ OutputCapturer/OutputCapturerProgram.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+
+namespace OutputCapturer
+{
+    class OutputCapturerProgram
+    {
+        static void Main(string[] args)
+        {
+            string solutionPath = Path.GetFullPath(typeof(OutputCapturerProgram).GetTypeInfo().Assembly.Location + "../../../../../..");
+            string assemblyPath = Path.GetFullPath(solutionPath + "/OutputGenerator/bin/Debug/netcoreapp1.1/OutputGenerator.dll");
+
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"\"{assemblyPath}\"",
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+            };
+
+            Process process = new Process { StartInfo = startInfo };
+
+            process.OutputDataReceived += new DataReceivedEventHandler(
+                (sender, eventArgs) =>
+                {
+                    if (eventArgs.Data != null)
+                    {
+                        Thread.Sleep(200);
+                        Debug.WriteLine(eventArgs.Data);
+                    }
+                }
+            );
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            process.Dispose();
+        }
+    }
+}
---
+++ OutputGenerator/OutputGeneratorProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ProcessOutputTest
+{
+    public class OutputGeneratorProgram
+    {
+        static void Main(string[] args)
+        {
+            LoggerFactory loggerFactory = new LoggerFactory();
+            loggerFactory.
+                AddConsole(LogLevel.Debug);
+            ILogger logger = loggerFactory.CreateLogger("test");
+
+            for (int i = 0; i < 1000; i++)
+            {
+                logger.LogDebug("{0} test test test test test test test test test test test", i);
+            }
+
+            loggerFactory.Dispose();
+        }
+    }
+}
